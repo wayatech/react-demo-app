38dfc2cbc63cfee034615d244f4af8dbee7d0292
Charly <charly@clic84.fr>
2020-09-08 17:17:39 +0200

    ♻️ refactor entry point

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import 'todomvc-app-css/index.css'
+
+import Main from './components/Main'
+import reducer from './reducers'
+
 import * as serviceWorker from './serviceWorker'
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Main />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
